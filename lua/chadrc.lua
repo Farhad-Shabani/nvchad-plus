c37588f2e54cdbf0e7ba6d8be60d59f153ad4b4d
Farhad Shabani <Farhad.Shabani@gmail.com>
2025-03-01 11:52:03 -0800

    feat: custom file path for status bar

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,50 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
+local custom_file = function()
+  -- Handle separator style
+  local sep_style = require("nvconfig").ui.statusline.separator_style
+  local sep_icons = require("nvchad.stl.utils").separators
+  local separators = (type(sep_style) == "table" and sep_style) or sep_icons[sep_style]
+  local sep_r = separators["right"]
+
+  -- Get file path
+  local path = vim.fn.expand "%:~:."
+  local name = (path == "" and "Empty") or path
+
+  -- Get file icon
+  local icon = "󰈚"
+  if name ~= "Empty" then
+    local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+    if devicons_present then
+      local ft_icon = devicons.get_icon(name)
+      icon = (ft_icon ~= nil and ft_icon) or icon
+    end
+  end
+
+  -- Formatting adjustments
+  name = " " .. name .. (sep_style == "default" and " " or "")
+
+  -- Preserve background color of St_file
+  local bg_color = vim.fn.synIDattr(vim.fn.hlID "St_file", "bg")
+  vim.cmd("hi St_FilePath guifg=#808080 guibg=" .. bg_color)
+
+  return "%#St_file# " .. icon .. "%#St_FilePath#" .. name .. "%#St_file_sep#" .. sep_r
+end
+
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
   theme = "onedark",
+}
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+M.ui = {
+  statusline = {
+    order = { "mode", "custom_file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+    modules = { custom_file = custom_file },
+  },
 }
 
 M.nvdash = { load_on_startup = true }
